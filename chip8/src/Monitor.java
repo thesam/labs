bda93e5183e6c6cfe71afa4849606d289628be30
Samuel Moritz <samuel.moritz@gmail.com>
2018-01-25 20:54:17 +0100

    Draw from video memory, scaled

--- chip8/src/Monitor.java
+++ chip8/src/Monitor.java
@@ -1,11 +1,13 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class Monitor {
 
     private final JFrame jFrame;
     private final JPanel jPanel;
     private boolean[][] videoMemory;
+    private BufferedImage bufferedImage = new BufferedImage(64,32,BufferedImage.TYPE_3BYTE_BGR);
 
     public Monitor(boolean[][] videoMemory) {
         this.videoMemory = videoMemory;
@@ -14,7 +16,7 @@ public class Monitor {
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jFrame.setSize(640,320);
         jPanel = new ScreenPanel();
-        jPanel.setSize(640,320);
+        //jPanel.setSize(64,32);
         //jPanel.getGraphics().f
         jFrame.getContentPane().add(jPanel);
     }
@@ -30,13 +32,15 @@ public class Monitor {
             for (int x = 0; x < 64; x++) {
                 for (int y = 0; y < 32; y++) {
                     Graphics2D graphics2d = (Graphics2D) graphics;
-                    graphics2d.scale(10,10);
+                    Graphics imageGraphics = bufferedImage.getGraphics();
+                    //graphics2d.scale(2,2);
                     if (videoMemory[x][y]) {
-                        graphics.setColor(Color.BLACK);
+                        imageGraphics.setColor(Color.BLACK);
                     } else {
-                        graphics.setColor(Color.WHITE);
+                        imageGraphics.setColor(Color.WHITE);
                     }
-                    graphics.drawLine(x,y,x,y);
+                    imageGraphics.drawLine(x,y,x,y);
+                    graphics2d.drawImage(bufferedImage,0,0,640,320,null);
                 }
             }
         }
