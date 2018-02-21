d3799ea8fdab7f6364a1c743d44c9b16f746016d
Samuel Moritz <samuel.moritz@gmail.com>
2018-02-21 18:25:27 +0100

    Enter username and PW

--- takeout/index.js
+++ takeout/index.js
@@ -1,10 +1,18 @@
 const Nightmare = require('nightmare')
-const nightmare = Nightmare({ show: true })
+const nightmare = Nightmare({ show: true, openDevTools: true})
+
+const EMAIL = process.env.TAKEOUT_EMAIL || 'foo@gmail.com';
+const PW = process.env.TAKEOUT_PW || 'bar';
 
 nightmare
   .goto('https://takeout.google.com/settings/takeout')
-  // .type('#search_form_input_homepage', 'github nightmare')
-  // .click('#search_button_homepage')
+  .wait('#Email')
+  .type('#Email', EMAIL)
+  .click('#next')
+  .wait('#Passwd')
+  .type('#Passwd', PW)
+  .wait('#signIn')
+  .click('#signIn')
   // .wait('#r1-0 a.result__a')
   // .evaluate(() => document.querySelector('#r1-0 a.result__a').href)
   //.end()
