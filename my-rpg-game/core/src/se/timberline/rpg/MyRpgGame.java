f2d63ad965dfe9c69cd5501ae2fc40c81392bf20
Samuel Moritz <samuel.moritz@gmail.com>
2018-06-03 21:45:00 +0200

    Draw character sprite

--- my-rpg-game/core/src/se/timberline/rpg/MyRpgGame.java
+++ my-rpg-game/core/src/se/timberline/rpg/MyRpgGame.java
@@ -5,15 +5,20 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class MyRpgGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+	private Texture characterSheet;
+	private TextureRegion characterSprite;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		characterSheet = new Texture("gfx/character.png");
+		characterSprite = new TextureRegion(characterSheet, 1, 6, 15, 22);
 	}
 
 	@Override
@@ -21,7 +26,8 @@ public class MyRpgGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		//batch.draw(img, 0, 0);
+		batch.draw(characterSprite,0,0,150,220);
 		batch.end();
 	}
 	
