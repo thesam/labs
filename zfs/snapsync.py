5604d994c4403a75ee4277cc952284ad586635e7
root <root@localhost>
2019-01-04 20:31:08 +0100

    Run zfs send/recv

--- zfs/snapsync.py
+++ zfs/snapsync.py
@@ -9,8 +9,15 @@ def get_latest_snapshot(dataset):
 	result = subprocess.run(['zfs','list','-H','-o','name','-r','-t','snapshot',dataset], stdout=subprocess.PIPE)
 	snapshots = result.stdout.split(b'\n')
 	latest_snapshot = snapshots[-2]
-	return latest_snapshot
+	return latest_snapshot.decode("utf-8")
 
-print(get_latest_snapshot(src))
-print(get_latest_snapshot(dest))
+latest_src = get_latest_snapshot(src)
+latest_dest = get_latest_snapshot(dest)
+from_snap = src + '@' + latest_dest.split('@')[1]
+to_snap = src + '@' + latest_src.split('@')[1]
+print("From: " + from_snap)
+print("To: " + to_snap)
 
+shellcmd = 'zfs send -I ' + from_snap + ' ' + to_snap + ' | zfs recv ' + dest
+print('Running: ' + shellcmd)
+subprocess.run(shellcmd, shell=True)
